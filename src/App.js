e8d2c35ed5f5cfb66391ef21a69edc5fcfe7aafe
Anel Canto <ac@anelcanto.com>
2023-05-09 15:18:23 -1000

    Add click handler funciton to Square function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function Square({value}){
-  return <Square>{value}" />
+  function handleClick(){
+    console.log('click')
+  }
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 export default function Board() {
+
+
   return (
     <>
       <div className="board-row">
